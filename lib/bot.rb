63326086d8451a369b8ac8caee64fd08ce6b23d7
francis uloko <francisuloko@yahoo.com>
2021-04-07 16:20:55 +0100

    Add channelstv world news article to bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,6 +3,7 @@
 require 'telegram/bot'
 require_relative '../config/token'
 require_relative '../lib/bot'
+require_relative '../lib/channelstv'
 
 
 
@@ -30,11 +31,18 @@ Telegram::Bot::Client.run(BOT_TOKEN) do |bot|
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text:  start_message.to_s)
     when '/bbc'
-      bot.api.send_message(chat_id: message.chat.id, text:  'loading...')
+      bot.api.send_message(chat_id: message.chat.id, text:  'coming soon...')
     when '/cnn'
-      bot.api.send_message(chat_id: message.chat.id, text:  'loading...')
+      bot.api.send_message(chat_id: message.chat.id, text:  'coming soon...')
     when '/channels'
-      bot.api.send_message(chat_id: message.chat.id, text:  'loading...')
+      channels = Paddy::ChannelsTv.new
+      link = channels.news_links
+      bot.api.send_message(chat_id: message.chat.id, text:  link[0])
+      bot.listen do |message|
+        if message.text == '/next'
+          bot.api.send_message(chat_id: message.chat.id, text:  link.sample)
+        end
+      end
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text:  'See you again friend')
     end
--- lib/channelstv.rb
+++ lib/channelstv.rb
@@ -10,13 +10,11 @@ module Paddy
             @news_links = []
         end 
       
-        private
-
         def scrape
             @url = "https://www.channelstv.com/category/world-news"
             @html = HTTParty.get(@url)
             @obj = Nokogiri::HTML(@html)
-            @articles = obj.css('div.cat_page')
+            @articles = @obj.css('div.cat_page')
         end
 
         def news_links
@@ -24,9 +22,8 @@ module Paddy
           @articles.each do |article|
             @news_links << article.css('a')[0].attributes['href']
           end
+          @news_links
         end
-
-        public
     end
 end
 
@@ -35,11 +32,12 @@ def scrape
     latest_news = []
     url = "https://www.channelstv.com/category/world-news"
     html = HTTParty.get(url)
-    obj = Nokogiri::HTML(html)
+    obj = Nokogiri::HTML(html.body)
     articles = obj.css('div.cat_page')
 
     articles.each do |article|
         latest_news << article.css('a')[0].attributes['href']
     end
-    latest_news
 end
+
+# puts scrape
