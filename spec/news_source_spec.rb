365cdd359b1d253424b3ee65a7c6dfaf54b2ee7e
francis uloko <francisuloko@yahoo.com>
2021-04-08 01:27:35 +0100

    fix rubocop error

--- spec/news_source_spec.rb
+++ spec/news_source_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require_relative '../lib/news_source'
 
 RSpec.describe 'Paddy::NewsSource' do
   context '#news_link' do
     it 'returns array of news links' do
       src = Paddy::NewsSource.new
-      expect(src.news_links.is_a? Array).to eq(true)
+      expect(src.news_links.is_a?(Array)).to eq(true)
     end
   end
 
   context '#scrape' do
     it 'returns array of news article' do
       src = Paddy::NewsSource.new
-      expect(src.news_links.is_a? Array).to eq(true)
+      expect(src.news_links.is_a?(Array)).to eq(true)
     end
   end
 end
