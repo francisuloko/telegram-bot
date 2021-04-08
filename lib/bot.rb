fda8ee21bc2d34d4ca9a263a1747c8520ada6dc2
francis uloko <francisuloko@yahoo.com>
2021-04-08 11:59:32 +0100

    Add scrapper methods and bbc news

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,29 +6,24 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Hello...')
-    when '/bbc'
-      bot.api.send_message(chat_id: message.chat.id, text: 'coming soon...')
-    when '/cnn'
-      bot.api.send_message(chat_id: message.chat.id, text: 'coming soon..')
+      bot.api.send_message(chat_id: message.chat.id, text: "Hi")
     when '/channels'
-      channels = Paddy::ChannelsTv.new
+      channels = Paddy::NewsSource.new
       articles = channels.news_links
-      bot.api.send_message(chat_id: message.chat.id, text: article[i])
-      bot.api.send_message(chat_id: message.chat.id, text: '/next')
       i = 0
-      bot.listen do |msg|
-        if msg.text == '/next' && i < articles.length
+      bot.api.send_message(chat_id: message.chat.id, text: articles[i])
+      bot.api.send_message(chat_id: message.chat.id, text: '/next')
+      bot.listen do |message|
+        if message.text == '/next' && i < articles.length
           i += 1
-          bot.api.send_message(chat_id: msg.chat.id, text: articles[i])
-          bot.api.send_message(chat_id: msg.chat.id, text: '/next')
+          bot.api.send_message(chat_id: message.chat.id, text: articles[i])
+          bot.api.send_message(chat_id: message.chat.id, text: '/next')
+          break if i == articles.length
         elsif message.text == '/stop'
-          bot.api.send_message(chat_id: msg.chat.id, text: 'See you again friend')
+          bot.api.send_message(chat_id: message.chat.id, text: 'See you again friend')
           break
         end
-        break if i == articles.length
       end
     end
-    bot.api.send_message(chat_id: message.chat.id, text: 'Want more news, try /cnn or /bbc')
   end
 end
--- lib/news_source.rb
+++ lib/news_source.rb
@@ -4,27 +4,50 @@ require 'byebug'
 
 module Paddy
   class NewsSource
-    attr_reader :news_links
-
+    attr_reader :scrapper
     def initialize
       @news_links = []
     end
 
     def news_links
-      @articles = scrape
-      @articles.each do |article|
-        @news_links << article.css('a')[0].attributes['href']
-      end
-      @news_links
+      @news_links << bbc.flatten + channelstv.flatten
+      byebug
     end
 
-    private 
+    # private
 
-    def scrape
-      @url = 'https://www.channelstv.com/category/world-news'
-      @html = HTTParty.get(@url)
+    def scrapper(url, elem)
+      @html = HTTParty.get(url)
       @obj = Nokogiri::HTML(@html)
-      @articles = @obj.css('div.cat_page')
+      @articles = @obj.css(elem)
+      @articles
+    end
+    
+    def bbc
+      @news_array = []
+      @url = 'https://www.bbc.com'
+      @elem = 'a.block-link__overlay-link'
+      @articles = scrapper(@url, @elem)
+      @articles.each do |a|
+        @news_array << @url + @articles.css('a')[1].attributes['href'].value
+      end
+      @news_array
+    end
+    
+    def channelstv
+      @news_array = []
+      @url = 'https://www.channelstv.com/category/world-news'
+      @elem = 'div.cat_page'
+      @articles = scrapper(@url, @elem)
+      @articles.each do |a|
+        @news_array << @articles.css('a')[0].attributes['href']
+      end
+      @news_array
     end
   end
 end
+
+s = Paddy::NewsSource.new
+s.news_links
+s.bbc
+s.channelstv
